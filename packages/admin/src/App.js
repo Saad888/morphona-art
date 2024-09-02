85f0b56b3f960024c7df5c1afe93514d7a400ac6
Saad Taimoor <saadtaimoor8@gmail.com>
2024-09-02 16:17:27 -0700

    Adding placeholder text for websites

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Future site of Morophona's admin website.
         </p>
         <a
           className="App-link"
